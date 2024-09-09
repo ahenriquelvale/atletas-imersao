5b35609f7a0af52d91c9ed2e4ba6c74b6cf987ff
ahenriquelvale <91564561+ahenriquelvale@users.noreply.github.com>
2024-09-09 00:00:32 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,25 +1,48 @@
 function pesquisar() {
     // Selects the section element with the ID "resultados-pesquisa" where the search results will be displayed.
     let section = document.getElementById("resultados-pesquisa");
-  
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value.trim();
+    console.log(campoPesquisa);
+
+    if (!campoPesquisa){
+        section.innerHTML = "<p>O campo de pesquisa está vazio</p>";
+        return;
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
     // Initializes an empty string to store the generated HTML for the search results.
     let resultados = "";
-  
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
     // Iterates over each item in the 'dados' array (presumably containing data about search results).
     for (let dado of dados) {
-      // Concatenates HTML elements to create a new search result item.
-      // The item includes a heading with a link to the title, a description paragraph, and a link for more information.
-      resultados += `
-        <div class="item-resultado">
-          <h2>
-            <a href="#" target="_blank">${dado.titulo}</a>
-          </h2>
-          <p class="descricao-metas">${dado.descricao}</p>
-          <a href="${dado.link}" target="_blank">Mais informações</a>
-        </div>
-      `;
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // Concatenates HTML elements to create a new search result item.
+            // The item includes a heading with a link to the title, a description paragraph, and a link for more information.
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-metas">${dado.descricao}</p>
+                <a href="${dado.link}" target="_blank">Mais informações</a>
+            </div>
+            `;
+        }
+
     };
-  
+
+    if (!resultados){
+        resultados = "<p>Nada foi encontrado</p>";
+    }
+
     // Sets the inner HTML of the 'section' element to the generated results.
     section.innerHTML = resultados;
-  }
+}
--- dados.js
+++ dados.js
@@ -2,16 +2,19 @@ let dados = [
     {
         titulo: "Rayssa Leal",
         descricao: "Rayssa Leal, conhecida como 'Fadinha', é uma skatista brasileira que se tornou a mais jovem medalhista olímpica da história do Brasil. Com um estilo único e muita personalidade, ela conquistou o mundo do skate e inspirou uma nova geração de skatistas.",
-        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal"
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate street fada sk8 imperatriz"
     },
     {
         titulo: "Rebeca Andrade",
         descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou diversas medalhas em competições internacionais, incluindo os Jogos Olímpicos. Sua versatilidade e determinação a tornaram uma das principais atletas brasileiras.",
-        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade"
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica artística artistica ouro"
     },
     {
         titulo: "Beatriz Souza",
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou em diversas competições nacionais e internacionais. Sua técnica e força a colocaram entre as principais atletas do judô brasileiro.",
-        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza" 
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "ouro judô judoka"
     }
 ];
