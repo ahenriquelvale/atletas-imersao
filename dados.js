8766d2265f154b1caac3126290c46e0e9321503f
Antonio Henrique Lima do Vale <antonio.henrique@tce.pi.gov.br>
2024-09-09 11:36:51 -0300

    Inclusão de dados de novos atletas

--- dados.js
+++ dados.js
@@ -16,5 +16,23 @@ let dados = [
         descricao: "Beatriz Souza é uma judoca brasileira que se destacou em diversas competições nacionais e internacionais. Sua técnica e força a colocaram entre as principais atletas do judô brasileiro.",
         link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
         tags: "ouro judô judoka"
+    },
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um dos principais surfistas brasileiros, campeão mundial da WSL. Conhecido por suas manobras radicais e carisma, ele é um dos maiores nomes do surf mundial.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags: "surf surfing surfe wsl radical manobras"
+    },
+    {
+        titulo: "Ítalo Ferreira",
+        descricao: "Ítalo Ferreira é um surfista brasileiro de destaque, campeão mundial da WSL. Com um estilo único e muita habilidade, ele conquistou diversos títulos importantes. Primeiro campeão olímpico do Surf, foi medalha de ouro nas olimpíadas de Tokyo 2020",
+        link: "https://pt.wikipedia.org/wiki/Ítalo_Ferreira",
+        tags: "surf surfe surfing wsl habilidade"
+    },
+    {
+        titulo: "Tatiana Weston-Webb",
+        descricao: "Tatiana Weston-Webb é a primeira surfista brasileira a conquistar um título mundial da WSL. Sua versatilidade e força a colocaram entre as principais atletas do surf feminino mundial. Foi medalha de prata nas olimpíadas de Paris 2024",
+        link: "https://pt.wikipedia.org/wiki/Tatiana_Weston-Webb",
+        tags: "surf surfe surfing wsl feminina"
     }
 ];
